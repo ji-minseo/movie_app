23a403d3c6d9f02c4677d48eb4218e2c5bb3db72
ji-minseo <eldkfnrkz@naver.com>
2020-03-30 20:36:39 +0900

    Updates

---
+++ precache-manifest.2f8ed17de2190d305c293e252fcd3cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65aa104522ef19765193d1752ad92af5",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ab3ea391a114df4294db",
+    "url": "/movie_app/static/css/main.49c60f8e.chunk.css"
+  },
+  {
+    "revision": "975c1a2c1dfdbd009111",
+    "url": "/movie_app/static/js/2.abca4018.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.abca4018.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab3ea391a114df4294db",
+    "url": "/movie_app/static/js/main.7c155fbb.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
